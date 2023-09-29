c1d34d596911f6cabb7c848bee99bdabd9995294
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-09-29 14:42:23 +1000

    ci: Add pyproject.toml + init_env.dev.sh script

---
+++ scripts/init_env.dev.sh
@@ -0,0 +1,63 @@
+#!/bin/zsh
+# This script initialises a python virtual environment.
+# It expects you to have Python installed at the system-level uses it for the virtual env.
+# It also checks whether your system's OS and Architecture to install the appropriate dependant extras.
+# All other extras and groups are installed (e.g. viz, dev)
+# For any issues:
+# + try to run this script from the project root level.
+
+set -e
+
+HELP="./$(basename $0) <virutal-env-name>"
+
+if [[ -z $1 ]]; then
+  echo "-- $HELP" >&2
+  exit 1
+fi
+
+VENV_DIR=$(realpath "$1")
+POETRY_FILE=$(realpath "pyproject.toml")
+OS=$(uname -o)
+ARCH=$(uname -m)
+
+echo "PYPROJECT=$POETRY_FILE"
+echo "OS=$OS"
+echo "ARCH=$ARCH"
+echo "PYTHON=$(which python3)"
+echo "++ Virtual environment will be installed at $VENV_DIR"
+
+if [[ ! -f $POETRY_FILE ]]; then
+  echo "-- Missing $POETRY_FILE." >&2
+  exit 1
+fi
+
+if [[ -d "$VENV_DIR" ]]; then
+  printf "-- Virtual environment $VENV_DIR already exists. Replace(y/n)? "
+  read x
+  [[ $x != 'y' ]] && echo "Exited." && exit 0
+  rm -rf "$VENV_DIR"
+fi
+
+echo "++ Creating virtual env at $VENV_DIR..."
+python3 -m venv $VENV_DIR
+
+echo "++ Activating virtual env..."
+source $VENV_DIR/bin/activate
+
+echo "++ Installing dependencies..."
+echo "++ Found poetry config file."
+pip install --upgrade pip
+pip install poetry
+
+echo "++ Removing poetry.lock..."
+rm -f poetry.lock
+echo "++ Installing dependencies..."
+if [[ ${ARCH:u} == ARM* && ${OS:u} == "DARWIN" ]]; then   #:u - uppercase (zsh only)
+  poetry install --with "dev" --extras "viz" --extras "apple"
+else
+  poetry install --with "dev" --extras "viz"
+fi
+
+echo "++ Done. Your virtual env is installed at $VENV_DIR"
+echo "To activate your virtual env run: source $VENV_DIR/bin/activate"
+exit 0
