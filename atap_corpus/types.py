cc4bda9febe84ef88f566b2f753fab1ae2eb85c3
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-10-03 16:01:43 +1100

    feat(types): Add TDocs

--- atap_corpus/types.py
+++ atap_corpus/types.py
@@ -3,5 +3,6 @@ from os import PathLike
 import spacy.tokens
 
 TMask = 'pd.Series[bool]'
-TPathLike = PathLike[str] | PathLike[bytes]
 TDoc = str | spacy.tokens.Doc
+TDocs = 'pd.Series[TDoc]'
+TPathLike = PathLike[str] | PathLike[bytes]
