f0a102cc8d6b9a870e1fd3b728ebf227b955f060
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-10-05 10:37:41 +1100

    feat(corpus): uniquename - Add errors + dev comments

--- atap_corpus/corpus/mixins.py
+++ atap_corpus/corpus/mixins.py
@@ -1,6 +1,14 @@
+import uuid
 from abc import abstractmethod
 
 import coolname
+import logging
+
+logger = logging.getLogger(__name__)
+
+_NAME_LEN = 2
+_MAX_COMBINATIONS = coolname.get_combinations_count(_NAME_LEN)
+_CURRENT_COUNT = 0
 
 
 class UniqueNameProviderMixin(object):
@@ -11,6 +19,13 @@ class UniqueNameProviderMixin(object):
         raise NotImplementedError()
 
     def unique_name(self) -> str:
-        while name := coolname.generate_slug(2):
+        global _CURRENT_COUNT, _MAX_COMBINATIONS
+        while name := coolname.generate_slug(_NAME_LEN):
+            if _CURRENT_COUNT >= _MAX_COMBINATIONS:
+                logger.debug(f"exhausted names from the coolname package with maximum={_MAX_COMBINATIONS}.")
+                # dev - this will probably never happen (len=2, combinations=320289), if it does then increase _NAME_LEN
+                break
             if not self.is_unique_name(name):
+                _CURRENT_COUNT += 1
                 return name
+        raise RuntimeError("unique names exhausted.")
