986ed73d36762b1bc5401ec5d122d2f49b150007
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-10-05 10:53:01 +1100

    tests(gcorpora): Add del corpus test

--- tests/test_gcorpora.py
+++ tests/test_gcorpora.py
@@ -1,7 +1,12 @@
 """ Test Global Corpora
 
-This Test Suite tests for:
+GlobalCorpora is a runtime feature of the module.
+It keeps track of all the BaseCorpus instances that are created so that we have a
+central management object.
+
+This test suite ensures core global corpora behaviours.
 1. All Corpus are added into the GlobalCorpora.
+2. When a Corpus reference is dropped, it is also dropped from GlobalCorpora.
 """
 
 from unittest import TestCase
@@ -11,10 +16,19 @@ from atap_corpus.registry import _Global_Corpora
 
 
 class TestGlobalCorpora(TestCase):
-    def test_given_subclass_dfcorpus_then_corpus_is_added_to_global_corpora(self):
+    def setUp(self):
         assert len(_Global_Corpora) == 0, "Global Corpora did not start empty."
+
+    def test_given_subclass_dfcorpus_then_corpus_is_added_to_global_corpora(self):
         dfcorpus = MockDataFrameCorpus()
         assert len(_Global_Corpora) == 1, f"{dfcorpus} was not added to GlobalCorpora."
 
         gc_dfcorpus = _Global_Corpora.get(dfcorpus.name)
         assert gc_dfcorpus is not None, f"Expecting {dfcorpus} in GlobalCorpora. Got {gc_dfcorpus}."
+
+    def test_given_subclass_dfcorpus_ref_is_deleted_then_gcorpora_removes_it(self):
+        dfcorpus = MockDataFrameCorpus()
+        assert len(_Global_Corpora) == 1, f"{dfcorpus} was not added to GlobalCorpora."
+
+        del dfcorpus
+        assert len(_Global_Corpora) == 0, "Expecting dfcorpus to be removed but it wasn't."
