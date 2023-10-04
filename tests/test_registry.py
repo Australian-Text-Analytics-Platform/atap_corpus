205a68b3243a0f334160eff10debef7d73174b8d
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-10-05 09:48:15 +1100

    tests(registry): Add test global corpora

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -9,3 +9,8 @@ class TestRegistry(TestCase):
         from atap_corpus.registry import _Unique_Name_Provider
         assert isinstance(_Unique_Name_Provider, UniqueNameProviderMixin), \
             f"_Unique_Name_Provider does have {UniqueNameProviderMixin.__name__}."
+
+    def test_global_corpora(self):
+        from atap_corpus.registry import _Global_Corpora
+        from atap_corpus.corpus.corpora import _GlobalCorpora
+        assert isinstance(_Global_Corpora, _GlobalCorpora), f"Only supports GlobalCorpora at the moment."
