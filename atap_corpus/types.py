d182b60bd0057013fb0e386fa26ccee1cdc6bfc9
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-10-05 10:38:44 +1100

    feat(types): import pandas

--- atap_corpus/types.py
+++ atap_corpus/types.py
@@ -1,9 +1,10 @@
 import os
 from typing import TypeAlias
 
+import pandas as pd
 import spacy.tokens
 
 Mask: TypeAlias = 'pd.Series[bool]'
 Doc: TypeAlias = str | spacy.tokens.Doc
 Docs: TypeAlias = 'pd.Series[Doc]'
-PathLike: TypeAlias = os.PathLike[str] | os.PathLike[bytes]
+PathLike: TypeAlias = str | os.PathLike[str]
