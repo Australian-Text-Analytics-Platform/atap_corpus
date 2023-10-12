5c8dbad4824705465e501fd2d8805ba0a6b82277
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-10-12 16:11:44 +1100

    doc(types): Add comments for Mask alias

--- atap_corpus/types.py
+++ atap_corpus/types.py
@@ -4,7 +4,7 @@ from typing import TypeAlias
 import pandas as pd
 import spacy.tokens
 
-Mask: TypeAlias = 'pd.Series[bool]'
+Mask: TypeAlias = 'pd.Series[bool]'  # amend to this type alias as necessary.
 Doc: TypeAlias = str | spacy.tokens.Doc
 Docs: TypeAlias = 'pd.Series[Doc]'
 PathLike: TypeAlias = str | os.PathLike[str]
