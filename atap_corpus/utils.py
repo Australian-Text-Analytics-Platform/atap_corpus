3860f317520785630040a1038275f46f90fea36d
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-10-24 10:11:46 +1100

    feat(utils): format_dunder_str - ensures str(arg)

--- atap_corpus/utils.py
+++ atap_corpus/utils.py
@@ -11,7 +11,7 @@ def format_dunder_str(cls: Type[Any], *args, **kwargs) -> str:
     Example returned string:
     <class_name arg0,arg1 key0=value0, key1=value1>
     """
-    _args: str = ",".join(args)
+    _args: str = ",".join((str(a) for a in args))
     _kwargs: str = ",".join([f"{k}: {v}" for k, v in kwargs.items()])
     return f"<{cls.__name__} {_args} {_kwargs}>"
 
