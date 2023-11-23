f77a5cd960b1b9c632e9f0f62a0553a36c582737
huen.chan@sydney.edu.au <huen.chan@sydney.edu.au>
2023-11-23 15:48:02 +1100

    feat(utils): Add download, corpus_uploader() - adhoc

--- atap_corpus/utils.py
+++ atap_corpus/utils.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import Type, Any, Optional
 
 from atap_corpus._types import PathLike, TCorpus
+from atap_corpus.corpus import Corpus, Corpora
 
 
 def format_dunder_str(cls: Type[Any], *args, **kwargs) -> str:
@@ -38,22 +39,53 @@ _IS_JUPYTER = is_jupyter()
 
 def setup_loggers(path: Optional[PathLike] = None):
     if path is None: path = "./logging_conf.ini"
-    import logging.config
+    ?import logging.config
     # loads logging configuration file at root.
     logging.config.fileConfig(path)
     logger = logging.getLogger(__name__)
     logger.debug(f"Loggers configured with {path}")
 
 
+# --- Ad Hoc ----
+# below are ad hoc functions added as part of v0.1.x
+# pending Corpus Loader. HASS-227
+
+
 # note: this is an ad-hoc download function (jupyter only)
-def download(corpus: TCorpus) -> 'HTML':
+def download(corpus: TCorpus) -> 'FileDownload':
     if _IS_JUPYTER:
-        from IPython.display import HTML
-        path = corpus.name + ".zip"
+        import tempfile
+        import panel as pn
+        pn.extension()
+        tmpd = Path(tempfile.mkdtemp())
+        path = tmpd.joinpath(corpus.name + ".zip")
         path = Path(corpus.serialise(path))
-        import html
-        href = html.escape("./" + path.name)
         default_fname = path.name
-        return HTML(f'<a href=/files/{href} download={default_fname}>Download {default_fname}</a>')
+        return pn.widgets.FileDownload(file=path.__str__(), filename=default_fname,
+                                       icon='download')
     else:
         raise NotImplementedError("Download is only available in jupyter.")
+
+
+def corpus_uploader() -> tuple['pn.Column', Corpora]:
+    """ Creates and return a File Uploader for Corpus and a Corpora that stores them.
+    Each Corpus uploaded will be stored in the Corpora. There can be multiple selected or uploaded multiple times.
+    """
+    import panel as pn
+    import io
+    pn.extension()
+    finp = pn.widgets.FileInput(accept='.zip', multiple=True)
+    corpora = Corpora()
+
+    def on_upload(_):
+        for _bytes in finp.value:
+            b_io = io.BytesIO(_bytes)
+            corpus = Corpus.deserialise(b_io)
+            b_io.close()
+            corpora.add(corpus)
+
+    finp.param.watch(on_upload, 'value')
+    header = pn.pane.HTML("<h4>Upload Corpus</h4><span>Each upload will be added to the Corpora.</span>")
+    return pn.Column(header, finp), corpora
+
+# ------------------------------------------------------
